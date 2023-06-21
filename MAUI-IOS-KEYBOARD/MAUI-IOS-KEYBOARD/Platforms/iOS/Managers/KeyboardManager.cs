d557bc0c5d809bcad7f16082d43ff5a01e3236e5
Pavlo Datsiuk <pavlo.datsiuk>
2023-06-21 15:40:15 +0200

    added keyboard manager

---
+++ MAUI-IOS-KEYBOARD/MAUI-IOS-KEYBOARD/Platforms/iOS/Managers/KeyboardManager.cs
@@ -0,0 +1,42 @@
+﻿using GameController;
+using UIKit;
+
+namespace MAUI_IOS_KEYBOARD.Platforms.iOS.Managers
+{
+    public class KeyboardManager
+    {
+        public bool HasHardwareKeyboard { get; }
+
+        public KeyboardManager()
+        {
+            HasHardwareKeyboard = GCKeyboard.CoalescedKeyboard != null;
+        }
+
+        public void HideSoftKeyboard(View view)
+        {
+            if (view != null)
+            {
+                var nativeView = view.Handler?.PlatformView as UIView;
+                if (nativeView != null)
+                    nativeView.EndEditing(true);
+            }
+            else
+            {
+                UIApplication.SharedApplication.KeyWindow.EndEditing(true);
+            }
+
+            UIApplication.SharedApplication.KeyWindow.BecomeFirstResponder();
+        }
+
+        public void ShowSoftKeyboard(View view)
+        {
+            if (view != null)
+            {
+                var nativeView = view.Handler?.PlatformView as UIView;
+                if (nativeView != null)
+                    nativeView.BecomeFirstResponder();
+            }
+        }
+
+    }
+}
